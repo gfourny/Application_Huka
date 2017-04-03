ac432750902c6193c4425984e1fe7625fbb1cfee
Fourny <fourny.g@free.fr>
2017-04-03 19:24:16 +0200

    maj

--- People/People/MainPage.xaml.cs
+++ People/People/MainPage.xaml.cs
@@ -3,10 +3,11 @@ using People.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Xamarin.Forms;
 
 namespace People
 {
-    public partial class MainPage
+    public partial class MainPage : Page
     {
         public MainPage()
         {
@@ -14,13 +15,20 @@ namespace People
             App.AlimentRepo.CreateDatabase();
             
         }
-        
+
+
         public async void OnGetButtonClicked(object sender, EventArgs args)
         {
             ObservableCollection<Aliments> aliments = 
                 new ObservableCollection<Aliments>(
                     await App.AlimentRepo.GetAllAlimentsAsync());
             peopleList.ItemsSource = aliments;
+        }
+
+        private async void aliment_Clicked(object sender, EventArgs e)
+        {
+            //ObservableCollection<Aliments> aliments = new ObservableCollection<Aliments>(await App.AlimentRepo.GetAllAlimentsAsync());
+            //this.peopleList = new ListView();
         }
     }
 }
