7d9b26d4077300f2668de18f3d3c3baafc9a58c3
danyCorbineau <dany.corbineau@ynov.com>
2017-04-04 09:32:06 +0200

    Add files via upload

---
+++ People/People.Droid/SplashActivity.cs
@@ -0,0 +1,13 @@
+﻿using Android.App;
+using Android.Support.V7.App;
+using huka_splash.Droid;
+
+[Activity(Label = "Huka", Icon = "@drawable/icon", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
+public class SplashActivity : AppCompatActivity
+{
+    protected override void OnResume()
+    {
+        base.OnResume();
+        StartActivity(typeof(MainActivity));
+    }
+}
